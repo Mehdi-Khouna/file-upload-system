2c7166283e843ea84a49cc67401d3fb897213f34
Mehdi KHOUNA <m.khouna@case-tunisia.com>
2025-11-05 20:39:50 +0100

    Backend integration complete

---
+++ frontend/src/app/services/upload.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Upload {
+  private baseUrl = 'http://localhost:3000/files'; // we'll update later
+
+  constructor(private http: HttpClient) {}
+
+  
+
+  /*uploadFile(){
+    console.log('File is uploaded')
+  }*/
+ uploadFile(file : File) : Observable<{key : string}> {
+  const formData = new FormData();
+  formData.append('file', file);
+  return this.http.post<{key : string}>(`${this.baseUrl}/upload`, formData);
+ }
+
+ getFileUrl(key : string): Observable<{url : string}>{
+  return this.http.get<{url : string}>(`${this.baseUrl}/${key}/url`);
+ }
+}
--- frontend/src/app/upload-card/upload-card.ts
+++ frontend/src/app/upload-card/upload-card.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Upload } from '../services/upload';
 
 @Component({
   selector: 'app-upload-card',
@@ -6,12 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload-card.scss'],
 })
 export class UploadCard {
+  constructor(private readonly uploadService : Upload) {}
   filename: string | null = null;
+  fileUrl : string | null = null;
+  file : File | null = null;
+  isUploading = false;
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
-      this.filename = input.files[0].name;
+      this.file = input.files[0];
+      this.filename = this.file.name;
+      this.fileUrl = null;
     }
+  }
+
+  onFileUpload() {
+    if (!this.file) return ;
+    this.isUploading = true;
+    this.uploadService.uploadFile(this.file).subscribe({
+      next: (response) => {
+        this.uploadService.getFileUrl(response.key).subscribe({
+          next : (res) => {
+            this.fileUrl = res.url;
+            this.isUploading = false;
+          },
+          error: () => (this.isUploading = false),
+        });
+      },
+      error : () => (this.isUploading = false),
+    });
   }
 }
